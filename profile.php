732d36e9e4cef006faac043a275b591a194b0648
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-06-03 22:58:29 +0200

    \r\n > \r since file is already LF ended

--- profile.php
+++ profile.php
@@ -57,10 +57,10 @@ $post['date'] = time();
 $post['text'] = <<<HTML
 [b]This[/b] is a [i]sample message.[/i] It shows how [u]your posts[/u] will look on the board.
 [quote=Anonymous][spoiler]Hello![/spoiler][/quote]
-[code]if (true) {\r\n
-	print "The world isn't broken.";\r\n
-} else {\r\n
-	print "Something is very wrong.";\r\n
+[code]if (true) {\r
+	print "The world isn't broken.";\r
+} else {\r
+	print "Something is very wrong.";\r
 }[/code]
 [irc]This is like code tags but without formatting.
 <Anonymous> I said something![/irc]
