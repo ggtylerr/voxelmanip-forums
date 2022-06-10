2ae1de7152626ffabf179dad23c05f313359f3a8
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-06-10 19:20:17 +0200

    Add viewport

--- lib/common.php
+++ lib/common.php
@@ -137,9 +137,10 @@ function pageheader($pagetitle = '', $fid = null) {
 HTML;
 
 	?><!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
 		<title><?=$pagetitle.$boardtitle?></title>
 		<?=$meta?>
 		<link rel="stylesheet" href="theme/common.css">
