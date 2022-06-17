abb0c5b49f889767c4063f60b015b6d1d0e4daec
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-06-17 23:15:46 +0200

    Lessen the post length requirement

--- newreply.php
+++ newreply.php
@@ -26,10 +26,10 @@ if ($action == 'Submit') {
 		$error = "You can't double post until it's been at least one day!";
 	if ($lastpost['user'] == $loguser['id'] && $lastpost['date'] >= (time() - 2) && $loguser['powerlevel'] > 3)
 		$error = "You must wait 2 seconds before posting consecutively.";
+	if (strlen(trim($message)) < 20)
+		$error = "Your post is too short to be meaningful. Please try to write something longer or refrain from posting.";
 	if (strlen(trim($message)) == 0)
 		$error = "Your post is empty! Enter a message and try again.";
-	if (strlen(trim($message)) < 35)
-		$error = "Your post is too short to be meaningful. Please try to write something longer or refrain from posting.";
 
 	if (!$error) {
 		$sql->query("UPDATE users SET posts = posts + 1, lastpost = ? WHERE id = ?", [time(), $loguser['id']]);
